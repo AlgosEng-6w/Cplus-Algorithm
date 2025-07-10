63097909cf56362cea5c3e485cf51d0412064fa7
AlgosEng-6w <ashen4725@gmail.com>
2025-07-10 18:46:00 +0800

    1.2

--- Language/Chapter1/Section2.cpp
+++ Language/Chapter1/Section2.cpp
@@ -6,3 +6,15 @@ int main()
     
     return 0;
 }
+
+
+// Note
+
+int num_tries = 0;
+// 用asignment运算符（=）进行初始化，沿袭自C语言
+// 在对象为内置类型，或者对象可以单一值加以初始化时，没问题
+int num_tries(0);       // 构造函数语法，用于处理多值初始化
+#include <complex>
+complex<double> purei(0, 7);
+// 尖括号表示complex是一个模板类（template class）
+// template class允许在不指明data member类型的情况下定义class
